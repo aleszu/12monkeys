da36bde0395e6b08b483c004c8933e49782a6467
Erik Reyna <erik@erikreyna.com>
2015-07-11 19:23:54 -0400

    gotta fix ajax request

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,58 @@
-$(document).ready(function(e){
+$(document).ready(function(e) {
 
-    var source   = $("#post").html();
-    var template = Handlebars.compile(source);
-    console.log(template)
+  var context = [{
+    name: 'Mercury',
+    description: '1st to Sun'
+  }, {
+    name: 'Venus',
+    description: '2nd to Sun'
+  }, {
+    name: 'Earth',
+    description: '3rd to Sun'
+  }, {
+    name: 'Mars',
+    description: '4th to Sun'
+  }, {
+    name: 'Jupiter',
+    description: '5th to Sun'
+  }, {
+    name: 'Saturn',
+    description: '6th to Sun'
+  }, {
+    name: 'Uranus',
+    description: '7th to Sun'
+  }, {
+    name: 'Neptune',
+    description: '8th to Sun'
+  }, ];
 
-    $('#results')
-        .append(template)
-        .addClass('covers')
-        .css('background-image', 'url("https://placehold.it/1600x1600")');
+  var source = $("#post").html();
+  var template = Handlebars.compile(source);
 
+  $('#results')
+    .append(template(context));
+
+  $('.post')
+    .css('background-image', 'url("https://placehold.it/1600x1600")');
+
+  // $.get('http://45.55.253.92/post', function(data) {
+  //   console.log(data);
+  // });
+
+  var data = {
+    "url": "http://www.nbcnews.com/storyline/confederate-flag-furor/confederate-flag-lowered-forever-south-carolina-capitol-n389996"
+  };
+
+  $.ajax({
+    method: "POST",
+    url: "http://45.55.253.92/",
+    data: data.url,
+  }).done(function() {
+    console.log("done");
+  }).fail(function() {
+    console.log("fail");
+  })
+  .always(function() {
+    console.log("always");
+  });
 });
