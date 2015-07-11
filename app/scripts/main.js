3a57b4b8b493849aa97d49c5caaec710e3c44db3
Erik Reyna <erik@erikreyna.com>
2015-07-11 16:37:41 -0400

    reimplementing form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(e){
+
+    $('.post')
+        .addClass('covers')
+        .css('background-image', 'url("https://placehold.it/1600x1600")');
+});
