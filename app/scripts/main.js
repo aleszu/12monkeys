b88c9b896d2c15b0e61fc2e1bb2d795a650a4e8e
Erik Reyna <erik@erikreyna.com>
2015-07-12 12:54:48 -0400

    need to make it pretty now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,6 @@ $(document).ready(function(e) {
   var source = $("#post").html();
   var template = Handlebars.compile(source);
   var placeholder = $('#results');
-
   // this is the id of the form
   $("#idForm").submit(function(event) {
     event.preventDefault();
@@ -14,15 +13,26 @@ $(document).ready(function(e) {
         type: "POST",
         url: url,
         data: $("#idForm").serialize(),
-    }).done(function(response) {
+      }).done(function(response) {
         console.log(response)
         data = JSON.parse(response);
         console.log(data);
 
         placeholder.html(template(data));
 
+        $('img').each(function() {
+          var img = new Image(),
+            self = this;
 
-    }).fail(function(e) {
+          img.onerror = function() {
+            $(self).prop('src', 'images/full_story.jpg');
+          }
+
+          img.src = this.src;
+        });
+
+
+      }).fail(function(e) {
         console.log(e);
         console.log("fail");
       })
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -32,14 +32,8 @@ function lint(files, options) {
       .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
   };
 }
-const testLintOptions = {
-  env: {
-    mocha: true
-  }
-};
 
 gulp.task('lint', lint('app/scripts/**/*.js'));
-gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 
 gulp.task('html', ['styles'], () => {
   const assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
@@ -123,21 +117,6 @@ gulp.task('serve:dist', () => {
   });
 });
 
-gulp.task('serve:test', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-
-  gulp.watch('test/spec/**/*.js').on('change', reload);
-  gulp.watch('test/spec/**/*.js', ['lint:test']);
 });
 
 // inject bower components
