2e8cb3f3d595a0731c1d218c11ebf0596f855e41
Erik Reyna <erik@erikreyna.com>
2015-07-12 11:48:51 -0400

    pushing for Ross

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,58 +1,35 @@
 $(document).ready(function(e) {
 
-  var context = [{
-    name: 'Mercury',
-    description: '1st to Sun'
-  }, {
-    name: 'Venus',
-    description: '2nd to Sun'
-  }, {
-    name: 'Earth',
-    description: '3rd to Sun'
-  }, {
-    name: 'Mars',
-    description: '4th to Sun'
-  }, {
-    name: 'Jupiter',
-    description: '5th to Sun'
-  }, {
-    name: 'Saturn',
-    description: '6th to Sun'
-  }, {
-    name: 'Uranus',
-    description: '7th to Sun'
-  }, {
-    name: 'Neptune',
-    description: '8th to Sun'
-  }, ];
-
   var source = $("#post").html();
   var template = Handlebars.compile(source);
+  var placeholder = $('#results');
 
-  $('#results')
-    .append(template(context));
+  // this is the id of the form
+  $("#idForm").submit(function(event) {
+    event.preventDefault();
 
-  $('.post')
-    .css('background-image', 'url("https://placehold.it/1600x1600")');
+    var url = "http://45.55.253.92/post";
 
-  // $.get('http://45.55.253.92/post', function(data) {
-  //   console.log(data);
-  // });
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: $("#idForm").serialize(),
+    }).done(function(response) {
+        console.log(response)
+        data = JSON.parse(response);
+        console.log(data);
 
-  var data = {
-    "url": "http://www.nbcnews.com/storyline/confederate-flag-furor/confederate-flag-lowered-forever-south-carolina-capitol-n389996"
-  };
+        placeholder.html(template(data));
 
-  $.ajax({
-    method: "POST",
-    url: "http://45.55.253.92/",
-    data: data.url,
-  }).done(function() {
-    console.log("done");
-  }).fail(function() {
-    console.log("fail");
-  })
-  .always(function() {
-    console.log("always");
+
+    }).fail(function(e) {
+        console.log(e);
+        console.log("fail");
+      })
+      .always(function() {
+        console.log("always");
+      });
+
+    return false;
   });
 });
